8b4c5751f78800ab583513be229d035bcfcb6863
adair.grassi <adair.grassi@useall.com.bt>
2023-10-05 13:07:13 -0300

    feat/CRUD accommodations finished

--- src/components/accommodations/AcommodationTable.tsx
+++ src/components/accommodations/AcommodationTable.tsx
@@ -2,10 +2,13 @@ import GenericLoading from "@components/base/GenericLoading";
 import GenericTable from "@components/base/GenericTable";
 import useFilterData from "@hooks/useFilterData";
 import useGetAccommodations from "@hooks/useGetAccommodations";
-// import { PencilSimple } from "phosphor-react";
 import { useEffect, useState } from "react";
 import AccommodationWindow from "./AcommodationWindow";
-import { PencilSimple } from "phosphor-react";
+import { PencilSimple, TrashSimple } from "phosphor-react";
+import api from "@utils/api";
+import { toast } from "sonner";
+import { queryClient } from "@utils/queryClient";
+import { useMutation } from "@tanstack/react-query";
 
 type Props = {
   searchText?: string;
@@ -34,28 +37,60 @@ const AccommodationTable = ({ searchText, reload }: Props) => {
     if (reload) refetch().then();
   }, [reload, refetch]);
 
+  const mutationDelete = useMutation(
+    (s?: number) => api.delete(`Acomodacao/DeletarArea/${s}`),
+    {
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(["getAccommodations"]);
+        toast.success("Acomodação exluída com sucesso!");
+      },
+    }
+  );
+
   if (isLoading) return <GenericLoading size={60} />;
   if (error) return <div>ERRO</div>;
   if (!accommodations?.length)
     return <div>Não existem acomodações cadastradas</div>;
 
   const tableValuesWithIcons = (filteredAccommodations ?? accommodations).map(
-    (accommodation) => ({
-      "": <PencilSimple className="cursor-pointer" size={24} />,
+    (accommodation: AccommodationType) => ({
+      Editar: (
+        <PencilSimple
+          onClick={() => {
+            setSelectedAccommodation(accommodation);
+            setIsOpen(true);
+          }}
+          className="cursor-pointer"
+          size={24}
+        />
+      ),
       ...accommodation,
+      Excluir: (
+        <TrashSimple
+          onClick={() => deleteAccommodation(accommodation.id)}
+          className="cursor-pointer text-red-500"
+          size={24}
+        />
+      ),
     })
   );
+
+  const deleteAccommodation = (id?: number) => {
+    if (!id) return;
+
+    const confirmDelete = confirm(
+      "Tem certeza que deseja excluir esta acomodação?"
+    );
+
+    if (!confirmDelete) return;
+
+    mutationDelete.mutate(id);
+  };
 
   return (
     <>
       <GenericTable
-        // values={filteredAccommodations ?? accommodations}
         values={tableValuesWithIcons}
-        // columns={Object.keys(tableValuesWithIcons[0])}
-        onClickRow={(p) => {
-          setSelectedAccommodation(p);
-          setIsOpen(true);
-        }}
         columns={Object.keys(tableValuesWithIcons[0])}
       />
       <AccommodationWindow
