e26348b439cbfaacc1b8102f0e31756c0c7214d7
vinicios.santos <viniciossantos@useall.com.br>
2023-10-17 19:37:19 -0300

    feat - crud menu

---
+++ src/types/MenuType.ts
@@ -0,0 +1,11 @@
+export type MenuType = {
+    id?: number;
+    descricao: string;
+    subMenus: SubMenusType[];
+}
+
+export type SubMenusType = {
+    id?: number,
+    codigoSubMenu?: number,
+    seq?: number
+}
