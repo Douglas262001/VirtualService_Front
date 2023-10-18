2da5117804a204dfd5fb97cc0f50fda8a67ea847
adair.grassi <adair.grassi@useall.com.bt>
2023-10-18 10:52:03 -0300

    a

--- src/pages/Menus/index.tsx
+++ src/pages/Menus/index.tsx
@@ -1,37 +1,5 @@
-import "./index.modules.css";
-
 const Menus = () => {
-  return (
-    <div className="h-[calc(100vh-4.5rem)] w-full p-2 flex flex-col">
-      <div className="header-options">
-        <p className="text-2xl ...">Menus</p>
-      </div>
-      <label
-        htmlFor="my-modal-student"
-        className="btn modal-button btn-secondary buttons-menu-menu"
-      >
-        Menu
-      </label>
-      <label
-        htmlFor="my-modal-student"
-        className="btn modal-button btn-secondary buttons-menu-menu"
-      >
-        Categorias
-      </label>
-      <label
-        htmlFor="my-modal-student"
-        className="btn modal-button btn-secondary buttons-menu-menu"
-      >
-        Itens
-      </label>
-      <label
-        htmlFor="my-modal-student"
-        className="btn modal-button btn-secondary buttons-menu-menu"
-      >
-        Etapas
-      </label>
-    </div>
-  );
+  return <div></div>
 };
 
 export default Menus;
