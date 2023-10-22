e75d7bf01cb5559f64fd87dba4f8902ea833d300
Vinicios-santos <viniciossantos-@hotmail.com>
2023-10-21 22:20:48 -0300

    xesq

--- src/components/caixa/itens/ComandasHeader.tsx
+++ src/components/caixa/itens/ComandasHeader.tsx
@@ -4,8 +4,11 @@ const ComandasHeader = () => {
   return (
     <div className="h-16 bg-[#27272a] mb-2  ml-8 mr-12 rounded-md flex justify-between items-center p-4">
       <h2 className="text-2xl">Comandas</h2>
-      <button className="btn btn-primary h-2">
+      <button className="btn btn-info h-2">
         Agrupar comandas <SubtractSquare size={24} />
+      </button>
+      <button className="btn btn-info h-2">
+        Lançar pedido <SubtractSquare size={24} />
       </button>
     </div>
   );
