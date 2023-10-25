7912c99ce84b94e39118be8dccb2e8f2448ddf0c
Vinicios-santos <viniciossantos-@hotmail.com>
2023-10-25 01:17:50 -0300

    signal r layout

--- src/components/base/layout/index.tsx
+++ src/components/base/layout/index.tsx
@@ -1,10 +1,19 @@
 import TopBar from "@components/layout/TopBar";
 import Sidebar from "@components/layout/Sidebar";
 import { Outlet } from "react-router-dom";
-import { Toaster } from "sonner";
+import { Toaster, toast } from "sonner";
 import { PrivateProvider } from "context/private";
-
+import Connector, { EnumAcaoHub } from "@utils/signalR";
+import { useEffect } from "react";
 export const Layout = () => {
+    const { events } = Connector();
+  
+    useEffect(() => {
+      events((acao: EnumAcaoHub) =>
+        toast.success('Chamando garçom')
+      );
+    });
+
   return (
     <PrivateProvider>
       <section className="flex  h-screen">
