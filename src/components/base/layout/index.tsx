1a8aeaf6f51a09bb4c8f6aeb9a223b6265652458
bruno leite <brunoleiteem@gmail.com>
2023-10-25 23:27:01 -0300

    (feat): signalr

--- src/components/base/layout/index.tsx
+++ src/components/base/layout/index.tsx
@@ -1,18 +1,19 @@
 import TopBar from "@components/layout/TopBar";
 import Sidebar from "@components/layout/Sidebar";
 import { Outlet } from "react-router-dom";
-import { Toaster, toast } from "sonner";
+import { Toaster } from "sonner";
 import { PrivateProvider } from "context/private";
-import Connector, { EnumAcaoHub } from "@utils/signalR";
+import Connector from "@utils/signalR";
 import { useEffect } from "react";
+import { notificationHandler } from "@utils/notifications";
 export const Layout = () => {
-    const { events } = Connector();
-  
-    useEffect(() => {
-      events((acao: EnumAcaoHub) =>
-        toast.success('Chamando garçom')
-      );
+  const { events } = Connector();
+
+  useEffect(() => {
+    events((data) => {
+      notificationHandler(data);
     });
+  });
 
   return (
     <PrivateProvider>
